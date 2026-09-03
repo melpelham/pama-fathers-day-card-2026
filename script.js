d2d5b204b5c22570959b6d0c8a202af0fb0b6f91
melpelham <melissajpelham@gmail.com>
2026-09-03 16:29:33 +0930

    Format

--- script.js
+++ script.js
@@ -1,108 +1,48 @@
 const envelope = document.getElementById("envelope");
-
 const card = document.getElementById("card");
-
 const cardWrapper =
     document.getElementById("cardWrapper");
-
 const letters =
     document.getElementById("letters");
-
 const instructions =
     document.getElementById("instructions");
 
-
 let envelopeOpened = false;
 
-
-/* =========================
-   OPEN ENVELOPE
-========================= */
-
 envelope.addEventListener("click", function () {
-
     if (envelopeOpened) {
         return;
     }
-
     envelopeOpened = true;
-
     envelope.classList.add("open");
-
     instructions.textContent =
         "Watch the card come out 💌";
-
-
-    /*
-        Halfway through the animation
-        the card reaches the top.
-
-        At this point we move the
-        envelope front BEHIND the card.
-
-        This means the card can smoothly
-        come back down in front of it.
-    */
-
     setTimeout(function () {
-
         envelope.classList.add(
             "card-coming-down"
         );
-
         cardWrapper.style.zIndex = "6";
-
     }, 1750);
-
-
-    /*
-        Card has completely finished
-        moving.
-
-        NOW animate PAMA.
-    */
-
     setTimeout(function () {
-
         letters.classList.add(
             "animate"
         );
-
         instructions.textContent =
             "Tap the card to open 💕";
-
     }, 3500);
-
 });
 
-
-/* =========================
-   OPEN CARD
-========================= */
-
 card.addEventListener("click", function (event) {
-
     event.stopPropagation();
-
-
     if (!envelopeOpened) {
         return;
     }
-
-
     card.classList.toggle("open");
-
-
     if (card.classList.contains("open")) {
-
         instructions.textContent =
             "Tap the card to close 💕";
-
     } else {
-
         instructions.textContent =
             "Tap the card to open 💕";
-
     }
-
 });
